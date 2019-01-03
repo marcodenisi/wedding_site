f6c4477764a400dbc5212a95d61c4e6f0c2893d1
Marco <marco@MacBook-Pro-di-Flavia.local>
2019-01-03 19:09:19 +0100

    fix radio button, add lambda call when submitting form

---
+++ js/handle_rsvp.js
@@ -0,0 +1,29 @@
+$("#rsvp_form").submit(function (event) {
+
+    console.info("Calling lambda")
+
+    /* stop form from submitting normally */
+    event.preventDefault();
+
+    let is_present = "false";
+    if (document.getElementById('gridRadios1').checked) {
+        is_present = "true";
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "https://rtr69l0ps7.execute-api.eu-west-1.amazonaws.com/test/post",
+        crossDomain: true,
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            name: $('#inputName').val(),
+            surname: $('#inputSurname').val(),
+            confirm: is_present,
+            participants: $('#inlineFormCustomSelectPref').val(),
+            notes: $('#inputNotes').val()
+        })
+    }).then(function (data) {
+        console.info(data);
+    });
+});
